7e2c970287db533f5340daecb089dcc4e152e680
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2022-04-26 23:51:35 -0700

    completed photolist

---
+++ src/components/PhotoList/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import { render, cleanup } from "@testing-library/react";
+import React from "react";
+import '@testing-library/jest-dom/extend-expect'
+import PhotoList from '..'
+
+afterEach(cleanup)
+
+describe('PhotoList is rendering', () => {
+    it('renders', () => {
+        render(<PhotoList/>)
+    });
+
+    it('renders fragment', () => {
+        const {asFragment} = render(<PhotoList/>)
+        expect(asFragment()).toMatchSnapshot()
+    } );
+});
